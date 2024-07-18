53bc310c94cebd5add985d058a80f5c8d84e99d0
MateuszMlodecki <mateuszmlodecki@icloud.com>
2024-07-18 20:52:01 +0200

    added working localStorage, added styles for all buttons

--- todo-list/script.js
+++ todo-list/script.js
@@ -8,6 +8,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const input = document.getElementById("todo-input");
   const ul = document.getElementById("todo-ul");
 
+  loadToDoItems();
+
   form.addEventListener("submit", function (event) {
     event.preventDefault();
     let toDoText = input.value;
@@ -15,21 +17,41 @@ document.addEventListener("DOMContentLoaded", () => {
     if (toDoText !== "") {
       addToDoItem(toDoText);
       input.value = "";
+      saveToDoItems();
     }
   });
 
   function addToDoItem(text) {
     const li = document.createElement("li");
     li.textContent = text;
+    li.className = "liMargin";
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "x";
+    deleteButton.className = "myButtonX";
     deleteButton.addEventListener("click", function () {
       deleteToDoItem(li);
     });
     li.appendChild(deleteButton);
     ul.appendChild(li);
   }
+
   function deleteToDoItem(li) {
     ul.removeChild(li);
+    saveToDoItems();
+  }
+
+  function saveToDoItems() {
+    const items = [];
+    ul.querySelectorAll("li").forEach((li) => {
+      items.push(li.firstChild.textContent);
+    });
+    localStorage.setItem("todoItems", JSON.stringify(items));
+  }
+
+  function loadToDoItems() {
+    const items = JSON.parse(localStorage.getItem("todoItems")) || [];
+    items.forEach((item) => {
+      addToDoItem(item);
+    });
   }
 });
