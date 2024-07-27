d89893de05004d4c6386d52dbc1bf981b4795c29
MateuszMlodecki <mateuszmlodecki@icloud.com>
2024-07-27 18:26:45 +0200

    added togglebutton for dark mode on main page

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function goToToDo() {
 function goToCalculator() {
   window.location.href = "calculator/index.html";
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const toggle = document.getElementById("darkModeToggle");
+  toggle.addEventListener("change", function () {
+    if (this.checked) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  });
+});
