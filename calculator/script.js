114e12151ef9f0365a927a2e76a9d22476660b15
MateuszMlodecki <mateuszmlodecki@icloud.com>
2024-07-28 16:15:09 +0200

    added logic for calculator

--- calculator/script.js
+++ calculator/script.js
@@ -1,3 +1,16 @@
 function goToHome() {
   window.location.href = "../index.html";
 }
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  display.value = eval(display.value);
+}
